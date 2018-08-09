4e01103953382a8a70506caaca60c24805dc3dd8
Javier Machin <javier_machin_acosta@hotmail.com>
2018-08-09 17:20:34 +0100

    Fix comments typo

--- scripts/script.js
+++ scripts/script.js
@@ -39,7 +39,7 @@ const game = (() => {
     scoreContainer.appendChild(node);
   }
 
-  //Implement check if winner with some / every
+  // Checks if the board contains any winning line
   function lookForWinner(gameBoard) {
     const winningLines = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],
                           [1,4,7],[2,5,8],[0,4,8],[2,4,6]];
